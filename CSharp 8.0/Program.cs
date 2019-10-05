3f5007f51616fe510c5d70428334e96c9c3d547e
Habib ul Rehman <mhabibulrehman@gmail.com>
2019-10-05 22:09:32 +0500

    Add project files.

---
+++ CSharp 8.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharp_8._0
+{
+    class Program
+    {
+        //Asynchrounous Streams
+        public static async IAsyncEnumerable<int> GenerateSequence()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+        public static async Task PrintSequence()
+        {
+            await foreach (var number in GenerateSequence())
+            {
+                Console.WriteLine(number);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintSequence().Wait();
+        }
+        
+    }
+}
